7e4844da4b15c18538e47fa509d3fc21a967a8aa
Charley <charleycyx@gmail.com>
2015-09-18 23:28:30 -0400

    Initial Commit

---
+++ Pebble/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Pebble
+//
+//  Created by Charley Chen on 9/18/15.
+//  Copyright © 2015 Charley Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
